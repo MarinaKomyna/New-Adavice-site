4466681bf4523e5b19ed24d1c2fb29479c11f345
Concorder <arenko.design@gmail.com>
2020-04-08 19:31:04 +0300

    pop_up added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-console.log('ready')
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    	if ($('#popup')) {
+			setTimeout(show_pop, 2e3);
+			function show_pop(){
+				$('#popup').fadeIn(900);
+			}
+			function close_pop() {
+				$('#popup').fadeOut(900);
+			}
+			$('#close_pop').on('click', close_pop);
+			$('#close_pop2').on('click', close_pop);
+	}
+
+});
